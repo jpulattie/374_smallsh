51ce92d98875a9be4ed289c6c7fffd20cb4318cc
jpulattie <97932025+jpulattie@users.noreply.github.com>
2025-02-15 17:30:37 +0000

    loop prob

--- pulattij_assignment4.c
+++ pulattij_assignment4.c
@@ -4,7 +4,7 @@
  * Do fix memory leaks and any additional issues you find.
  */
 
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -13,14 +13,14 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
 #define INPUT_LENGTH 2048
-#define MAX_ARGS		 512
+#define MAX_ARGS 512
 
+int opened_new_out = 0;
+int opened_new_in = 0;
 int new_out;
 int new_in;
 int exit_status;
-char executable[INPUT_LENGTH + 1];
 int running = 0;
 struct command_line
 {
@@ -29,94 +29,115 @@ struct command_line
 	char *input_file;
 	char *output_file;
 	bool is_bg;
+	bool is_internal;
 };
 
-int cd (char *directory){
+int cd(char *directory)
+{
 	char current_dir[256];
 	int size = 256;
 	getcwd(current_dir, sizeof(current_dir));
-	//printf("current directory after check: %s\n", current_dir);
-	if (directory == NULL) {
+	// printf("current directory after check: %s\n", current_dir);
+	if (directory == NULL)
+	{
 		directory = getenv("HOME");
 	}
-	if (strcmp(directory, current_dir) !=0) {
-		//printf("DIFFERENT DIRECTORY INDICATED\n");
-		//printf("directory: %s\n", directory);
-		//printf("current home: %s\n", current_dir);
-		if (chdir(directory) == 0) {
-			//printf("Changed directory to: %s\n", directory);
-    		} else {
-        		perror("chdir failed");
-    		};
-		//printf("checking...\n");
+	if (strcmp(directory, current_dir) != 0)
+	{
+		// printf("DIFFERENT DIRECTORY INDICATED\n");
+		// printf("directory: %s\n", directory);
+		// printf("current home: %s\n", current_dir);
+		if (chdir(directory) == 0)
+		{
+			// printf("Changed directory to: %s\n", directory);
+		}
+		else
+		{
+			perror("chdir failed");
+		};
+		// printf("checking...\n");
 		getcwd(current_dir, sizeof(current_dir));
-		//printf("current directory after check: %s\n", current_dir);
+		// printf("current directory after check: %s\n", current_dir);
 	}
-	//printf("Directory: %s\n", directory);
-	// do the change directory stuffs up here
+	// printf("Directory: %s\n", directory);
+	//  do the change directory stuffs up here
 	return 0;
 }
 
-int status() {
-	
+int status()
+{
+
 	printf("exit value %d\n", exit_status);
 }
 
-int execute(struct command_line *ex){
-	pid_t spawnpid = -5; 
-	int childStatus; 
-	int childPid; 
+int execute(struct command_line *ex)
+{
+	pid_t spawnpid = -5;
+	int childStatus;
+	int childPid;
 
-	
-	//printf("executable command function: %s\n", ex);
 	spawnpid = fork();
-	
-	switch (spawnpid) {
-		case -1:
-			perror("fork() failed!"); 
-			exit_status = 1;
-			exit(1); 
-			break; 
-		case 0:
-			//printf("I am the child. My pid = %d Going to sleep now!\n", getpid()); 
-			execvp(ex->argv[0], ex->argv);
-			close(new_out);
-			close(new_in);
-			perror("process failed");
-			exit_status =1;
-			//printf("exit status: %d\n", exit_status);
-			exit(1);
-			break;
-		default:
-			//printf("I am the parent. My pid = %d\n", getpid()); 
-			childPid = waitpid(spawnpid,&childStatus, 0); 
-			if(WIFEXITED(childStatus)) {
-				exit_status = WEXITSTATUS(childStatus);
-			}
-			close(new_out);
-			close(new_in);
-			//printf("Exit status is now: %d\n", exit_status);
-			//printf("Parent's waiting is done as the child with pid %d exited\n", childPid); 
-			break;  //
 
+	switch (spawnpid)
+	{
+	case -1:
+		perror("fork() failed!");
+		exit_status = 1;
+		exit(1);
+		break;
+	case 0:
+		// printf("I am the child. My pid = %d Going to sleep now!\n", getpid());
+		execvp(ex->argv[0], ex->argv);
+		close(new_out);
+		close(new_in);
+		perror("process failed");
+		exit_status = 1;
+		// printf("exit status: %d\n", exit_status);
+		exit(1);
+		break;
+	default:
+		// printf("I am the parent. My pid = %d\n", getpid());
+		childPid = waitpid(spawnpid, &childStatus, 0);
+		if (WIFEXITED(childStatus))
+		{
+			exit_status = WEXITSTATUS(childStatus);
+		}
+		if (opened_new_in == 1)
+		{
+			close(new_in);
+		}
+		if (opened_new_out == 1)
+		{
+			close(new_out);
+		}
+
+		// exit(0);
+		// printf("Exit status is now: %d\n", exit_status);
+		// printf("Parent's waiting is done as the child with pid %d exited\n", childPid);
+		break; //
 	}
-		printf("here\n");
-	return 0;
 }
 
 struct command_line *parse_input()
 {
 	char input[INPUT_LENGTH];
-	struct command_line *curr_command = (struct command_line *) calloc(1, sizeof(struct command_line));
+	struct command_line *curr_command = (struct command_line *)calloc(1, sizeof(struct command_line));
 	int command_count = 0;
 	char get_direc[256];
-	int hashtags = 0;
+	int iterations = 0;
 	char *cd_home;
 	int loop = 1;
 	int breaker = 0;
+
+	printf("SHOULD BE BLANK COMMAND:\n. first arg: %s\n. arg count:%d\n. in: %s\n. out: %s\n", curr_command->argv[0], curr_command->argc, curr_command->input_file, curr_command->output_file);
+
 	// Get input
 	printf(": ");
 	fflush(stdout);
+	// if (fgets(input, INPUT_LENGTH, stdin) == NULL) {
+	//	printf("\n\nError at input. Closing\n\n");
+	//	exit(1);
+	// }
 	fgets(input, INPUT_LENGTH, stdin);
 
 	// Tokenize the input
@@ -125,129 +146,159 @@ struct command_line *parse_input()
 	char *input_file_name = NULL;
 	char *output_file_name = NULL;
 
-
-	while(token && breaker ==0){
-		//printf("token top of while loop: %s\n", token);
-		if (token[0] == '#' && command_count == 0) {
-			//printf("found comment: %s\n", token);
-			while (token != NULL && (strcmp(token, "\n") != 0)) {
+	while (token && breaker == 0)
+	{
+		// printf("ITERATIONS: %d\n", iterations);
+		iterations = iterations + 1;
+		// printf("token top of while loop: %s\n", token);
+		if (token[0] == '#' && command_count == 0)
+		{
+			// printf("found comment: %s\n", token);
+			while (token != NULL && (strcmp(token, "\n") != 0))
+			{
 				token = strtok(NULL, "\n");
 			}
-		} else if ((token[0] != '#' || command_count != 0) && (strcmp(token, "exit")==0)) {
-			//printf("token: %s\n", token);
+		}
+		else if ((token[0] != '#' || command_count != 0) && (strcmp(token, "exit") == 0))
+		{
+			// printf("token: %s\n", token);
 			command_count++;
-			//printf("exit indicator: %d\n",(strcmp(token, "exit")==0));
+			curr_command->is_internal = true;
+			// printf("exit indicator: %d\n",(strcmp(token, "exit")==0));
 			running = 1;
-			//printf("EXITING\n");
-			executable[0] = '\0';
-			breaker =1;
+			// printf("EXITING\n");
+			breaker = 1;
 			break;
-		} else if (strcmp(token, "cd") == 0) {
-			//printf("CATCH HERE\n");
-			//cd_home = getenv("HOME");
-			//getcwd(get_direc, sizeof(get_direc));
-			//printf("Before cd runs, current working directory is: %s\n", get_direc);
-			//printf("cd is: %s\n", cd_home);
+		}
+		else if (strcmp(token, "cd") == 0)
+		{
+			// printf("CATCH HERE\n");
+			// cd_home = getenv("HOME");
+			// getcwd(get_direc, sizeof(get_direc));
+			// printf("Before cd runs, current working directory is: %s\n", get_direc);
+			// printf("cd is: %s\n", cd_home);
+			curr_command->is_internal = true;
 			token = strtok(NULL, " \n");
 
-			while (token != NULL && (strcmp(token, "\n") != 0)) {
-				//printf("Loop %d start\n", loop);
-				//printf("token after CD: %s\n", token);
+			while (token != NULL && (strcmp(token, "\n") != 0))
+			{
+				// printf("Loop %d start\n", loop);
+				// printf("token after CD: %s\n", token);
 				directory = (char *)malloc(strlen(token) + 1);
 				directory = token;
-				//printf("directory string: %s\n", directory);
+				// printf("directory string: %s\n", directory);
 				token = strtok(NULL, " \n");
-				//printf("loop %d end\n", loop);
+				// printf("loop %d end\n", loop);
 			}
 			cd(directory);
 			breaker = 1;
-			executable[0] = '\0';
 			break;
-			// getenv('HOME')			
-			// open that directory 
-			// handle one more token after cd?  
+			// getenv('HOME')
+			// open that directory
+			// handle one more token after cd?
 			// maybe make this a separate function?????
 		}
-		else if(!strcmp(token,"<")){
-			curr_command->input_file = strdup(strtok(NULL," \n"));
-			//printf("command input catch: %s\n", curr_command->input_file);
+		else if (strcmp(token, "status") == 0)
+		{
+			curr_command->is_internal = true;
+			command_count++;
+			breaker = 1;
+			status();
+			break;
+		}
+		else if (!strcmp(token, "<"))
+		{
+			curr_command->input_file = strdup(strtok(NULL, " \n"));
+			// printf("command input catch: %s\n", curr_command->input_file);
 			input_file_name = (char *)malloc(strlen(curr_command->input_file) + 2);
-			//strcat(input_file_name, "\""); 
-			strcat(input_file_name, strdup(curr_command->input_file)); 
-			//strcat(input_file_name, "\""); 
-			printf("input file name: %s\n", input_file_name);
+			// strcat(input_file_name, "\"");
+			strcat(input_file_name, strdup(curr_command->input_file));
+			// strcat(input_file_name, "\"");
+			// printf("input file name: %s\n", input_file_name);
 			int open_new_input = open(input_file_name, O_RDONLY);
-			if (open_new_input == -1 ) {
+			if (open_new_input == -1)
+			{
 				perror("error opening file:");
 				exit_status = 1;
 				break;
-			} 
-			else {
+			}
+			else
+			{
+				opened_new_in = 1;
 				new_in = dup2(open_new_input, 0);
-			  }
-			command_count++;
-			
-		} else if(!strcmp(token,">")){
-			curr_command->output_file = strdup(strtok(NULL," \n"));
-			//printf("command output catch: %s\n", curr_command->output_file);
-			output_file_name = (char *)malloc(strlen(curr_command->output_file) + 2);
-			//strcat(output_file_name, "\""); 
-			strcat(output_file_name, strdup(curr_command->output_file));
-			//strcat(output_file_name, "\""); 
-			//printf("output file name: %s\n", output_file_name);
-			int open_new_output = open(output_file_name, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-			if (open_new_output == -1 ) {
-				perror("error opening file:");
-				exit_status = 1;
-				break;
-			} 
-			else {
-				new_out = dup2(open_new_output, 1);
-			  }
-			command_count++;
-			
-		} else if(!strcmp(token,"&")){
-			curr_command->is_bg = true;
-			command_count++;
-			
-		} else {
-			curr_command->argv[curr_command->argc++] = strdup(token);
-			if (strlen(executable) == 0){
-				//printf("blank executable\n");
-				strcat(executable, strdup(token)); 
-			} else if (strlen(executable) !=0 ) {
-				strcat(executable, " "); 
-				strcat(executable, strdup(token)); 
 			}
 			command_count++;
 		}
-		token=strtok(NULL," \n");
-		//printf("running number: %d\n", running);
-		//printf("Command Count: %d\n", command_count);
+		else if (!strcmp(token, ">"))
+		{
+			curr_command->output_file = strdup(strtok(NULL, " \n"));
+			// printf("command output catch: %s\n", curr_command->output_file);
+			output_file_name = (char *)malloc(strlen(curr_command->output_file) + 2);
+			// strcat(output_file_name, "\"");
+			strcat(output_file_name, strdup(curr_command->output_file));
+			// strcat(output_file_name, "\"");
+			// printf("output file name: %s\n", output_file_name);
+			int open_new_output = open(output_file_name, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+			if (open_new_output == -1)
+			{
+				perror("error opening file:");
+				exit_status = 1;
+				break;
+			}
+			else
+			{
+				opened_new_out = 1;
+
+				new_out = dup2(open_new_output, 1);
+			}
+			command_count++;
+		}
+		else if (!strcmp(token, "&"))
+		{
+			curr_command->is_bg = true;
+			command_count++;
+		}
+		else
+		{
+			curr_command->argv[curr_command->argc++] = strdup(token);
+			command_count++;
+		}
+		token = strtok(NULL, " \n");
+		// printf("running number: %d\n", running);
+		// printf("Command Count: %d\n", command_count);
 	}
-	
-	printf("ready to execute -%s-\n", executable);
+
+	printf("ready to execute -%s-\n", curr_command->argv[0]);
 	printf("curr input: %s | curr output: %s\n", input_file_name, output_file_name);
-	if (strcmp(executable, "status") == 0) {
-		status();
-	} else if (strlen(executable) > 0){
-	execute(curr_command);}
-	printf("resetting executable\n");
-	executable[0] = '\0';
-	printf("executable is now -%s-\n", executable);
+	// if (curr_command->argv[0] && (strcmp(curr_command->argv[0], "status") == 0))
+	//{
+	//	status();
+	// }
 	return curr_command;
 }
-
 
 int main()
 {
 	struct command_line *curr_command;
+	int i = 0;
 
-	while(running != 1)
+	while (running != 1 && i < 11)
 	{
-		//printf("running # in main: %d\n", running );
+		// printf("running # in main: %d\n", running );
 		curr_command = parse_input();
-
-	}
+		printf("sent back to main\n");
+		printf("Current command:\n. first arg: %s\n. arg count:%d\n. in: %s\n. out: %s\n", curr_command->argv[0], curr_command->argc, curr_command->input_file, curr_command->output_file);
+		if (curr_command->argc > 0 && curr_command->is_internal != true)
+		{
+			execute(curr_command);
+			printf("back from execute\n");
+		}
+		if (curr_command->is_internal == true)
+		{
+			printf("one of three built in commands\n");
+		}
+		i++;
+		}
+	
 	return EXIT_SUCCESS;
 }
